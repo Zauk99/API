dd4458772380fa5938a745a219a262b4f2464b23
Zauk99 <pablohd140605@gmail.com>
2025-11-19 13:07:04 +0100

    Version 4

--- src/main/java/gestion/tareas/api/config/SecurityConfig.java
+++ src/main/java/gestion/tareas/api/config/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig { 
 
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
